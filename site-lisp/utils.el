e3cc3c7d2f52547e9ce7c26255a30a5a2a814df8
Fred Campos <mail2frede@gmail.com>
2020-05-17 19:12:23 -0300

    fixed some utils functions

--- site-lisp/utils.el
+++ site-lisp/utils.el
@@ -12,41 +12,29 @@
   :type 'bool
   :group 'utils)
 
-
-(defcustom utils:kill-all-buffers-hooks (list)
-  "Hooks to be used when killing all buffers.  LIST."
-  :type 'list
-  :group 'utils)
-
-
-(defun utils:get-home-dir ()
-  "Retrieve dir from 'user-init-file'."
-  (file-name-directory user-init-file))
-
-
-(defun utils:get-template-directory ()
+(defun utils:--get-template-directory ()
   "Get template directory."
   (let (template-directory)
     (setq template-directory
-	  (file-name-as-directory (concat user-emacs-directory "templates")))))
+          (file-name-as-directory (concat user-emacs-directory "templates")))))
 
-
-(defun utils:get-snippets-directory ()
-  "Get snippets directory."
-
-  (let (snippets-directory)
-    (setq snippets-directory
-	  (file-name-as-directory (concat user-emacs-directory "snippets")))))
-
+(defun utils:kill-all-buffers ()
+  "Kill all buffers except for that are on list."
+  (interactive)
+  (let (not-to-kill-list)
+    (setq not-to-kill-list '("*scratch*"))
+    (dolist (current-buffer (buffer-list))
+      (if (member (buffer-name current-buffer) not-to-kill-list)
+          (bury-buffer)
+        (kill-buffer (buffer-name current-buffer))))))
 
 (defun utils:indent-region (N)
   "Indent region N."
   (interactive "p")
   (if (use-region-p)
       (progn (indent-rigidly (region-beginning) (region-end) (* N 4))
-	     (setq deactivate-mark nil))
+             (setq deactivate-mark nil))
     (self-insert-command N)))
-
 
 (defun utils:unindent-region (N)
   "Unindent region N."
@@ -54,9 +42,8 @@
   (interactive "p")
   (if (use-region-p)
       (progn (indent-rigidly (region-beginning) (region-end) (* N -4))
-	     (setq deactivate-mark nil))
+             (setq deactivate-mark nil))
     (self-insert-command N)))
-
 
 (defun utils:get-file-contents(fpath)
   "Return the contents of a file as a string.  FPATH."
@@ -72,18 +59,11 @@
   (indent-guide-mode utils:fl-blankspace)
   (if (eq utils:fl-blankspace -1)
       (progn
-	 (whitespace-mode utils:fl-blankspace)
-	 (setq utils:fl-blankspace +1))
+        (whitespace-mode utils:fl-blankspace)
+        (setq utils:fl-blankspace +1))
     (progn
       (whitespace-mode)
       (setq utils:fl-blankspace -1))))
-
-(defun utils:get-parent-dir (dirname-or-filename)
-  "Retrieve parent directory.  DIRNAME-OR-FILENAME."
-  (defvar parent-dir nil)
-  (setq parent-dir
-	(file-name-directory (directory-file-name dirname-or-filename)))
-  (expand-file-name parent-dir))
 
 (defun utils:new-buffer-frame ()
   "Create a new frame with a new empty buffer."
@@ -98,54 +78,51 @@
     (error "Project root not found or not configured, this mode will not work properly!"))
   (unless (file-exists-p (concat (projectile-project-root) ".dir-locals.el"))
     (let ((template-folder)
-	  (destination-folder)
-	  (destination-file)
-	  (template-files))
+          (destination-folder)
+          (destination-file)
+          (template-files))
       (setq destination-folder (projectile-project-root))
       (setq template-folder
-	    (file-name-as-directory (concat (utils:get-template-directory) language)))
+            (file-name-as-directory (concat (utils:--get-template-directory) language)))
       (setq template-files (cdr (cdr (directory-files template-folder))))
       (dolist (tpl-file template-files)
-	(setq destination-file
-	      (concat "." (replace-regexp-in-string "\.tmpl" "" tpl-file)))
-	(copy-file (concat template-folder tpl-file)
-		   (concat destination-folder destination-file) t)))))
-
+        (setq destination-file
+              (concat "." (replace-regexp-in-string "\.tmpl" "" tpl-file)))
+        (copy-file (concat template-folder tpl-file)
+                   (concat destination-folder destination-file) t)))))
 
 (defun utils:replace-string-in-file (dest-file pattern new-value-string)
   "Replace string in file.  DEST-FILE PATTERN NEW-VALUE-STRING."
   (let ((file-contents)
-	(tmp-file))
+        (tmp-file))
     (setq tmp-file (concat dest-file "-tmp"))
     (setq file-contents (replace-regexp-in-string
-			 pattern new-value-string (utils:get-file-contents dest-file) t))
+                         pattern new-value-string (utils:get-file-contents dest-file) t))
     (append-to-file file-contents nil tmp-file)
     (copy-file tmp-file dest-file t)
     (delete-file tmp-file nil)))
 
-
-(defun utils:proc--force-wait (process)
+(defun utils:--proc-force-wait (process)
   "Block until PROCESS exits successfully."
   (while (process-live-p process)
     (sit-for 0.1 t)))
 
-
 (defun utils:run-process(new-buffer proc-name &rest args)
   "Run sync PROCESS and show output on a new named BUFFER.  NEW-BUFFER PROC-NAME &REST ARGS."
   (let ((proc))
-      (setq proc (apply #'start-process "running-buffer-ps" new-buffer proc-name args))
-      (set-process-sentinel proc
-			    #'(lambda (p event)
-				(when (= 0 (process-exit-status p))
-				  (message "Process: %s had event '%s'" p event))))
-      (delete-other-windows)
-      (switch-to-buffer-other-window new-buffer)
-      (utils:proc--force-wait proc))
+    (setq proc (apply #'start-process "running-buffer-ps" new-buffer proc-name args))
+    (set-process-sentinel proc
+                          #'(lambda (p event)
+                              (when (= 0 (process-exit-status p))
+                                (message "Process: %s had event '%s'" p event))))
+    (delete-other-windows)
+    (switch-to-buffer-other-window new-buffer)
+    (utils:--proc-force-wait proc))
   (display-buffer (current-buffer)))
-
 
 (defun utils:set-project-root-on-current-directory ()
   "Include .projectile file on current-directory."
+  (interactive)
   (write-region "" nil (expand-file-name ".projectile")))
 
 (provide 'utils)
