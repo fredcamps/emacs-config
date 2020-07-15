b5361498bcae499135e8ba77d0a95a61ef32055f
Fred Campos <mail2frede@gmail.com>
2020-07-15 00:46:42 -0300

    fixed venv detection issues

--- local-init.el
+++ local-init.el
@@ -770,10 +770,9 @@
   :config
   (defun python:--replace-template-variables (dir-locals-file)
     "Replace variables from .dir-locals.el file.  DIR-LOCALS-FILE."
-    (with-eval-after-load "virtualenvwrapper"
-      (call-interactively #'venv-workon)
-      (utils:replace-string-in-file dir-locals-file
-                                    "{{ VENV-NAME }}" venv-current-name)))
+    (call-interactively #'venv-workon)
+    (utils:replace-string-in-file dir-locals-file
+                                  "{{ VENV-NAME }}" venv-current-name))
 
   (defun python:setup ()
     "Function that setups 'python-mode'."
@@ -802,7 +801,6 @@
       (unless (file-exists-p dir-locals-file)
         (utils:generate-project-files "python")
         (python:--replace-template-variables dir-locals-file)
-        (hack-local-variables)
         (python:setup)))))
 
 (use-package lsp-jedi
